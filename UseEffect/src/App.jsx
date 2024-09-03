cb3040108ce9645f2f1a546d55c3e560845dead1
AyushRamoliya <ayushramoliya69@gmail.com>
2024-09-03 18:00:05 +0530

    work

--- UseEffect/src/App.jsx
+++ UseEffect/src/App.jsx
@@ -1,11 +1,15 @@
 import React from 'react'
 import Ex2useeffect from './Components/Ex2useeffect'
 import './App.css';
+import Ex2 from './Components/Ex2';
+import Ex3blankdependency from './Components/Ex3blankdependency';
 
 export default function App() {
   return (
     <>
         <Ex2useeffect/>
+        <Ex2/>
+        <Ex3blankdependency/>
     </>
   )
 }
