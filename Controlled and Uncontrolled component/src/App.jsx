db998ca8bbc9b8e7ee8e76711f85c1a727f170da
AyushRamoliya <ayushramoliya69@gmail.com>
2024-09-13 13:43:41 +0530

    controlled and uncontrolled componentes

---
+++ Controlled and Uncontrolled component/src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import Controlled from './Componentes/Controlled'
+import Uncontrolled from './Componentes/Uncontrolled'
+import Formheandling from './Componentes/Formheandling'
+
+export default function App() {
+  return (
+    <>
+    
+      <Controlled/>
+
+      <Uncontrolled/>
+
+      <Formheandling/>
+    </>
+  )
+}
