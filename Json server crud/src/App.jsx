d2ea28f31ec564ed2363bc0b42a2e3cfa4141e76
AyushRamoliya <ayushramoliya69@gmail.com>
2024-10-22 13:52:47 +0530

    Json Server CRUD

---
+++ Json server crud/src/App.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import Apiwithaxios from './Componentes/Apiwithaxios'
+
+export default function App() {
+  return (
+    <div>
+      <Apiwithaxios/>
+    </div>
+  )
+}
